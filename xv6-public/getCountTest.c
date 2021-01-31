8255de1e2f59e9b1c4a5ee175bcd6b5fd484d7eb
mohammad <mjavad.ardestani00@gmail.com>
2021-02-01 02:44:58 +0330

    getCountTest added

---
+++ xv6-public/getCountTest.c
@@ -0,0 +1,16 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+
+int main(int argc, char *argv[]){
+    getpid();
+    getpid();
+    // printf(1,"%d",argv[argc-1]);
+    int y = 11;
+    int x =getCount(getpid(),y);
+    printf(1,"%d",x);
+
+
+exit();
+}
