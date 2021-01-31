1a74984b12ba063b4d57a6df533cd08f40edf485
mohammad <mjavad.ardestani00@gmail.com>
2021-02-01 02:44:11 +0330

    getCount in  proc.c edited

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -568,18 +568,19 @@ getChildren(void *ch_list, int curpid) {
 int 
 getCount(int pid, int syscallNum){
   struct proc *p;
-  acquire(&ptable.lock);
+  
   for (p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
     if (p->pid == pid) {
-      return p->sysCallCounter[syscallNum];
+      
       // cprintf("In the next line you can see how many times a syscall has been called:\n");
       // for (int i = 1; i <= 29; i++)
       // {
       //   cprintf("syscall(%d) --> %d time[s]\n",i,p->sysCallCounter[i]);
       // }
+    // cprintf("syscall(%d) --> %d time[s]\n",syscallNum,p->sysCallCounter[syscallNum]);
+    return p->sysCallCounter[syscallNum];
     }
   } 
-  release(&ptable.lock);
   return -1;
 }
 
