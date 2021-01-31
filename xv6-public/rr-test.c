f3ab71756d46282acdb88c5a3d0060b3fd2c83be
mahvash <mahvash.siavashpour@gmail.com>
2021-01-31 15:28:05 +0330

    state changes

---
+++ xv6-public/rr-test.c
@@ -0,0 +1,21 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(){
+
+    for(int i=0;i<10;i++){
+        int pid = fork();
+        if(pid == 0){
+            for(int j=0;j<1000;j++){
+                printf(1, "%d : %d\n", getpid, j);
+            }
+            return 0;
+        }else
+        {
+            continue;
+        }
+        
+    }
+    return 0;
+}
