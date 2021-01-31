3ab5b13a7f08f9a3762e77a8493703de0b447c03
mohammad <mjavad.ardestani00@gmail.com>
2021-01-31 15:02:53 +0330

    Makefile edited

--- xv6-public/getParentIDtest.c
+++ xv6-public/getParentIDtest.c
@@ -1,11 +1,13 @@
-#include <types.h>
-#include <stat.h>
-#include <user.h>
-#include <proc.h>
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
 
 int main(){
-    for(int i =0; i<5;i++){
-        fork();
-        printf("this is process %d and my parent ID is %d",getpid,getParentID());
-    }
+    // for(int i =0; i<5;i++){
+    //     if (fork() == 0){
+        printf(1,"this is process %d and my parent ID is %d\n",getpid(),getParentID());
+    //     }
+    // }
+    return 0;
 }
