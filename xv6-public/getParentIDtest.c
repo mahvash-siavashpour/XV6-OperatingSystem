9c6b869f92502303c9fd76223b7b91fda716b942
mohammad <mjavad.ardestani00@gmail.com>
2021-01-31 14:16:44 +0330

    getParentID added to Makefile

--- xv6-public/getParentIDtest.c
+++ xv6-public/getParentIDtest.c
@@ -1,7 +1,11 @@
 #include <types.h>
 #include <stat.h>
 #include <user.h>
+#include <proc.h>
 
 int main(){
-    
+    for(int i =0; i<5;i++){
+        fork();
+        printf("this is process %d and my parent ID is %d",getpid,getParentID());
+    }
 }
