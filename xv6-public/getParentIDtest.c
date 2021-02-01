be238edc551247d5bb5f02ebf1ffc87fca311d93
mohammad <mjavad.ardestani00@gmail.com>
2021-02-02 00:29:21 +0330

    getParentIDtets.c edited

--- xv6-public/getParentIDtest.c
+++ xv6-public/getParentIDtest.c
@@ -4,13 +4,20 @@
 
 
 int main(){
-    int ParentID = getpid();
-    printf(1,"this is parent process and my id is %d \n",ParentID);
-    if( fork() == 0){
-        int childID = getpid();
-        printf(1,"this is process %d and my parent ID is %d\n",childID,getParentID());
+    int number_of_children = 5;
+    int parentID = getpid();
+    printf(1,"this is parent process and my id is %d \n",parentID);
+    for(int i = 0; i<number_of_children; i++){
+        if(getpid() == parentID ) 
+            if(0 == fork()){
+                printf(1,"this is child %d and my parent is %d\n",getpid(),getParentID());
+                exit();
+            }
+        }
+    
+    for(int i=0; i< number_of_children; i++){
+        wait();
     }
-    wait();
 
     exit();
 }
